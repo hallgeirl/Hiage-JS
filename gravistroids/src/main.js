49d46ea24b6d20edaa46540da4ec8a28e25e72d3
Hallgeir Lien <hallgeir.lien@gmail.com>
2014-03-29 14:00:28 +0100

    Removed commented code

--- gravistroids/src/main.js
+++ gravistroids/src/main.js
@@ -13,16 +13,3 @@
 
     hiage.start();
 });
-//require(["hiage.js/init", "hiage.js/core/Game", "component/AllComponents"], function (init, Game, AllComponents) {
-    /*require(["Stage"],
-        function (Stage) {
-            var aspectRatio = screen.width / screen.height;
-            var game = new Game('container', 500, aspectRatio, 1, "data/resources.json");
-
-            var stage = new Stage(game.objectFactory, 500 * aspectRatio, 500, game.messageDispatcher);
-            game.pushState(stage);
-            stage.initialize();
-            game.start();
-        });
-})
-*/
